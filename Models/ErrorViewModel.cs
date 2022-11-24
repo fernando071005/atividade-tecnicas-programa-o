467a6ca3bb0edf6c426aaf70fa6d4f2abd890fb6
fernando <f.saraiva@uni9.edu.br>
2022-11-23 21:37:24 -0300

    controle

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace atividade_tecnicas_programa_o.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
